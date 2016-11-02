a3eaa9b3214ed6cfe477cb58d402e33fa0f5e784
Kenan Yildirim <kenan@kenany.me>
2016-11-02 15:30:02 -0400

    tools: set up eslint
    Closes https://github.com/KenanY/binomial-cdf/issues/11

--- test/index.js
+++ test/index.js
@@ -9,11 +9,11 @@ test('exports a function', function(t) {
 
 test('cdf', function(t) {
   t.plan(7);
-  t.equal(cdf(52, 100, 1/2), 0.691350293205374);
-  t.equal(cdf(0, 5, 1/4), 0.2373046875);
-  t.equal(cdf(1, 5, 1/4), 0.6328125);
-  t.equal(cdf(2, 5, 1/4), 0.896484375);
-  t.equal(cdf(3, 5, 1/4), 0.984375);
-  t.equal(cdf(4, 5, 1/4), 0.9990234375);
-  t.equal(cdf(5, 5, 1/4), 1);
+  t.equal(cdf(52, 100, 1 / 2), 0.691350293205374);
+  t.equal(cdf(0, 5, 1 / 4), 0.2373046875);
+  t.equal(cdf(1, 5, 1 / 4), 0.6328125);
+  t.equal(cdf(2, 5, 1 / 4), 0.896484375);
+  t.equal(cdf(3, 5, 1 / 4), 0.984375);
+  t.equal(cdf(4, 5, 1 / 4), 0.9990234375);
+  t.equal(cdf(5, 5, 1 / 4), 1);
 });
