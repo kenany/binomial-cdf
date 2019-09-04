579aced9196823b7dc34616fe3dec013ac0691c9
Kenan Yildirim <kenan@kenany.me>
2019-09-04 19:37:09 -0400

    deps: eslint@6.3.0, eslint-config@7.0.0
    Closes https://github.com/KenanY/binomial-cdf/pull/44
    Closes https://github.com/KenanY/binomial-cdf/pull/45

--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
-var cdf = require('../');
 var test = require('tape');
 var isFunction = require('lodash.isfunction');
+
+var cdf = require('../');
 
 test('exports a function', function(t) {
   t.plan(1);
