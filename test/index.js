bcfc0e3eeef451c42ff9daad95d807ab960392a9
Kenan Yildirim <kenan@kenany.me>
2022-01-13 16:35:44 -0500

    chore(deps): conventional-changelog-conventionalcommits@4.6.3
    Closes https://github.com/KenanY/binomial-cdf/pull/139

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
-var test = require('tape');
-var isFunction = require('lodash.isfunction');
+const test = require('tape');
+const isFunction = require('lodash.isfunction');
 
-var cdf = require('../');
+const cdf = require('../');
 
 test('exports a function', function(t) {
   t.plan(1);
